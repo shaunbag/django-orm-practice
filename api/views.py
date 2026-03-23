3ae3cbc7f504c9b392635e0a23bf9ba3b648fbd8
ShaunMcelhinney <shaunbag@hotmail.com>
2026-03-23 14:33:05 +0000

    rest_framework additions

---
+++ api/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+from bookstore.models import Book, Author, Rating
+from rest_framework import status, renderers
+from rest_framework.response import Response
+from .serializers import BookSerializer, AuthorSerializer, RatingsSerializer
+from rest_framework.decorators import api_view, action
+from rest_framework import viewsets
+from rest_framework.reverse import reverse
+
+@api_view(["GET"])
+def api_root(request):
+    return Response(
+        {
+            "books": reverse("book-view", request=request, format=None),
+            "authors": reverse("author-view", request=request, format=None),
+            "ratings": reverse("rating-view", request=request, format=None)
+        }
+    )
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def books(self, request, *args, **kwargs):
+        return Response(self.books)
+    
+
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def authors(self, request, *args,**kwargs):
+        return Response(self.authors)
+    
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
+
+
+class RatingsViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingsSerializer
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def ratings(self, request, *args, **kwargs):
+        return Response(self.ratings)
+    
+
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
--- bookstore/scripts/orm_script.py
+++ bookstore/scripts/orm_script.py
@@ -1,99 +1,105 @@
 from ..models import Book, Author, Rating
-from django.db.models import CharField, Max, Min, Avg, Count, Sum, Value
+from django.db.models import CharField, Max, Min, Avg, Count, Sum, Value, Prefetch
 from django.db import connection
 from pprint import pprint
 from django.db.models.functions import Length, Concat
 
 def run():
-    # print("starting the ORM script....\n")
-    # print("Starting with .all()\n")
-    # authors = Author.objects.all()
-    # books = Book.objects.all()
-    # ratings = Rating.objects.all()
+    print("starting the ORM script....\n")
+    print("Starting with .all()\n")
+    authors = Author.objects.all()
+    books = Book.objects.all()
+    ratings = Rating.objects.all()
    
-    # print("Authors: ")
-    # print(authors)
-    # print("\n")
-    # print("Books:")
-    # print(books)
-    # print("\n")
-    # print("Ratings:")
-    # print(ratings)
-    # print("\n")
-    # print("\nNext we have filter() for each Model\n")
+    print("Authors: ")
+    print(authors)
+    print("\n")
+    print("Books:")
+    print(books)
+    print("\n")
+    print("Ratings:")
+    print(ratings)
+    print("\n")
+    print("\nNext we have filter() for each Model\n")
 
-    # authors = Author.objects.filter(name__contains="R")
-    # books = Book.objects.filter(title__exact="Frankenstein")
-    # ratings = Rating.objects.filter(rating__gte=3).order_by("-rating")
+    authors = Author.objects.filter(name__contains="R")
+    books = Book.objects.filter(title__exact="Frankenstein")
+    ratings = Rating.objects.filter(rating__gte=3).order_by("-rating")
 
-    # print("Authors with name containing 'R': ")
-    # print(authors)
-    # print("\n")
-    # print("Books where title Exactly matches: ")
-    # print(books)
-    # print("\n")
-    # print("Ratings where rating is greater than or equal to 3: ")
-    # print(ratings)
-    # print("\n")
-    # print("\n next use some aggregations...\n")
+    print("Authors with name containing 'R': ")
+    print(authors)
+    print("\n")
+    print("Books where title Exactly matches: ")
+    print(books)
+    print("\n")
+    print("Ratings where rating is greater than or equal to 3: ")
+    print(ratings)
+    print("\n")
+    print("\n next use some aggregations...\n")
 
-    # authors = Author.objects.aggregate(age=Max("age"))
-    # books = Book.objects.aggregate(rating=Avg("ratings__rating"))
-    # ratings = Rating.objects.aggregate(rating=Min("rating"))
-    # ratings_all = Rating.objects.aggregate(
-    #     avg=Avg("rating"),
-    #     max=Max("rating"),
-    #     min=Min("rating"), 
-    #     count=Count("rating"), 
-    #     sum=Sum("rating")
-    #     )
+    authors = Author.objects.aggregate(age=Max("age"))
+    books = Book.objects.aggregate(rating=Avg("ratings__rating"))
+    ratings = Rating.objects.aggregate(rating=Min("rating"))
+    ratings_all = Rating.objects.aggregate(
+        avg=Avg("rating"),
+        max=Max("rating"),
+        min=Min("rating"), 
+        count=Count("rating"), 
+        sum=Sum("rating")
+        )
 
-    # print("Authors Max age: ")
-    # print(authors)
-    # print("\n")
-    # print("Books Avg rating for a book:")
-    # print(books)
-    # print("\n")
-    # print("Ratings min rating given:")
-    # print(ratings)
-    # print("\n")
-    # print("All Ratings aggregations:")
-    # print(ratings_all)
-    # print("\n")
-
-
-    # book = Book.objects.first()
-
-    # print(book.ratings.all())
+    print("Authors Max age: ")
+    print(authors)
+    print("\n")
+    print("Books Avg rating for a book:")
+    print(books)
+    print("\n")
+    print("Ratings min rating given:")
+    print(ratings)
+    print("\n")
+    print("All Ratings aggregations:")
+    print(ratings_all)
+    print("\n")
 
 
-    # # fetch all books and apply annotate on the length of the title
+    book = Book.objects.first()
 
-    # books = Book.objects.annotate(len_title=Length("title")).filter(
-    #     len_title__lte=30
-    # ).order_by("len_title")
+    print(book.ratings.all())
 
-    # print(books.values("title", "len_title")) 
-    # print("\n")
 
-    # # add a concatenated message to a model
+    # fetch all books and apply annotate on the length of the title
 
-    # concat = Concat("title", Value(": Avg Rating -> "), Avg("ratings__rating"), output_field=CharField())
+    books = Book.objects.annotate(len_title=Length("title")).filter(
+        len_title__lte=30
+    ).order_by("len_title")
 
-    # books = Book.objects.annotate(message=concat)
+    print(books.values("title", "len_title")) 
+    print("\n")
 
-    # for b in books:
-    #     print(b.message)
-    # print("\n")
+    # add a concatenated message to a model
 
-    # print([b.message for b in books])
+    concat = Concat("title", Value(": Avg Rating -> "), Avg("ratings__rating"), output_field=CharField())
+
+    books = Book.objects.annotate(message=concat)
+
+    for b in books:
+        print(b.message)
+    print("\n")
+
+    print([b.message for b in books])
+    print("\n")
     
 
     """
         Methods for solving the N+1 problem to optimise the queries and prefetch data and related
     """
 
-    books = Book.objects.prefetch_related('ratings', 'authors')
+    ratings = Prefetch(
+        'ratings',
+        queryset=Rating.objects.filter(rating__gte=4)
+    )
+
+    books = Book.objects.prefetch_related(ratings, 'authors')
 
     for b in books:
         print(f"-------{b.title}-------")
@@ -103,6 +109,7 @@ def run():
         for r in b.ratings.all():
             print(f"Rating: {r.rating}")
 
+    print("\n")
 
     rating = Rating.objects.only("rating", "book__title").select_related("book")
 
